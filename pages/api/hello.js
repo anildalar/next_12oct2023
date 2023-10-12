50128349ab28ea21f6419dfc1d51c24df1dc40a8
anildalar <dollorinfotech@gmail.com>
2023-10-12 09:57:48 +0530

    new

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,21 @@
+//1. Import Area
+
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
-export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+//Defination Area
+function handler(req, res) {
+        // function serialization
+ //object.method().method(actualAtguemnt); 
+                      //{ property:value } is a JS Object
+   res.status(200).json({ 
+                                msg1:`Hello1 ${req.query.name}`,
+                                msg2:`Hello4 ${req.query.surname}`,
+                                msg3:'Hello2' ,
+                                msg4:"Hello3 ",
+                                    //obj.property.property
+                                testkey: req.query.name
+                        });
 }
+
+//3. Export
+export default handler;
